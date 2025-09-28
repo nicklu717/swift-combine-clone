ee218da7c3766cd3711d6a2a03f73e9a4d71bd7e
Nick Lu <nicklu717@gmail.com>
2025-09-28 13:54:22 +0800

    refactor: make CurrentValueSubject inherit from PassthroughSubject

--- Sources/CombineClone/Subjects/CurrentValueSubject.swift
+++ Sources/CombineClone/Subjects/CurrentValueSubject.swift
@@ -5,21 +5,16 @@
 //  Created by 陸瑋恩 on 2025/9/28.
 //
 
-class CurrentValueSubject<Output>: Subject {
+class CurrentValueSubject<Output>: PassthroughSubject<Output> {
     private(set) var value: Output
-    
-    private var subscriptions: [Subscription<Output>] = []
     
     init(_ value: Output) {
         self.value = value
+        super.init()
     }
     
-    func sink(receiveValue: @escaping (Output) -> Void) {
-        self.subscriptions.append(Subscription(receiveValue: receiveValue))
-    }
-    
-    func send(_ value: Output) {
+    override func send(_ value: Output) {
         self.value = value
-        subscriptions.forEach { $0.receiveValue(value) }
+        super.send(value)
     }
 }
